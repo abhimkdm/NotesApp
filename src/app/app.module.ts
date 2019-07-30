f48f0c02498dc01d40212664f950bef48e582366
Abhishek <abhimkdm@gmail.com>
2019-07-30 07:38:18 +0530

    BootStrap
    Designing an Notes App Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SharedModule } from './Examples/shared.module';
+import { NavComponent } from './main/nav/nav.component';
+import { TagsComponent } from './main/tags/tags.component';
+import { NotesComponent } from './main/notes/notes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    TagsComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main/tags/tags.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'note-tags',
+  templateUrl: './tags.component.html',
+  styleUrls: ['./tags.component.css']
+})
+export class TagsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
